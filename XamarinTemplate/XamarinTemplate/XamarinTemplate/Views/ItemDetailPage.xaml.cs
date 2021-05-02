820d8d3faa63c8b4b0cbaa2eb4ccc51265f2d329
Fonsei <fonsei1988@msn.com>
2021-05-02 12:58:23 +0200

    Empty Project

---
+++ XamarinTemplate/XamarinTemplate/XamarinTemplate/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinTemplate.ViewModels;
+
+namespace XamarinTemplate.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
